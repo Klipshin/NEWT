f3b95b3cf986052ce4e2d1cc7144e805381bc0e2
Cliff Edward Alsonado <cliffedwardalsonado@gmail.com>
2025-12-01 16:52:50 +0800

    added intro animation, probably needs edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
 import 'pages/landing_page.dart';
 
 void main() {
@@ -26,7 +27,90 @@ class MyApp extends StatelessWidget {
       title: 'NEWT',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green, useMaterial3: true),
-      home: const LandingPage(),
+      home: const IntroVideoPage(),
+    );
+  }
+}
+
+class IntroVideoPage extends StatefulWidget {
+  const IntroVideoPage({super.key});
+
+  @override
+  State<IntroVideoPage> createState() => _IntroVideoPageState();
+}
+
+class _IntroVideoPageState extends State<IntroVideoPage> {
+  late VideoPlayerController _controller;
+  bool _isVideoInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeVideo();
+  }
+
+  Future<void> _initializeVideo() async {
+    // Load video from assets
+    _controller = VideoPlayerController.asset('assets/videos/1201.mp4');
+
+    await _controller.initialize();
+    setState(() {
+      _isVideoInitialized = true;
+    });
+
+    // Play the video
+    _controller.play();
+
+    // Listen for video completion
+    _controller.addListener(() {
+      if (_controller.value.isInitialized &&
+          _controller.value.position >= _controller.value.duration) {
+        _navigateToLandingPage();
+      }
+    });
+  }
+
+  void _navigateToLandingPage() {
+    // Ensure we don't navigate multiple times
+    _controller.pause();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const LandingPage()),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_isVideoInitialized) {
+      return const Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(child: CircularProgressIndicator(color: Colors.white)),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      // GestureDetector allows user to tap anywhere to skip video
+      body: GestureDetector(
+        onTap: _navigateToLandingPage,
+        child: SizedBox.expand(
+          child: FittedBox(
+            // BoxFit.cover ensures the video fills the screen
+            // (cropping edges if aspect ratio differs)
+            fit: BoxFit.cover,
+            child: SizedBox(
+              width: _controller.value.size.width,
+              height: _controller.value.size.height,
+              child: VideoPlayer(_controller),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
